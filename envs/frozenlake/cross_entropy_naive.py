75d02cba94efbe079fd28a746d0f010216282905
TM23-sanji <tusharmishra802@gmail.com>
2025-11-19 23:25:54 +0530

    v_iter_and_cross_entropy

---
+++ envs/frozenlake/cross_entropy_naive.py
@@ -0,0 +1,125 @@
+# this will perform bad
+import os, wandb, gymnasium as gym, torch, numpy as np
+import typing as tt, torch.nn as nn, torch.optim as optim
+from dotenv import load_dotenv
+from dataclasses import dataclass
+load_dotenv()
+api_key = os.getenv("WANDB_API_KEY")
+wandb.login(key=api_key)
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 16
+PERCENTILE = 70
+
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        shape = (env.observation_space.n, )
+        self.observation_space = gym.spaces.Box(0.0, 1.0, shape, dtype=np.float32)
+    
+    def observation(self, observation):
+        res = np.copy(self.observation_space.low)
+        res[observation] = 1.0
+        return res
+
+class Net(nn.Module):
+    def __init__(self, obs_size:int, hidden_size:int, n_actions:int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+    
+    def forward(self, x: torch.tensor):
+        return self.net(x)
+
+@dataclass
+class EpisodeStep:
+    observation : np.ndarray
+    action : int
+
+@dataclass
+class Episode:
+    reward : float
+    steps : tt.List[EpisodeStep]
+
+def iterate_batches(env: gym.Env, net: Net, batch_size: int) -> tt.Generator[tt.List[Episode], None, None]:
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs, _ = env.reset()
+    sm = nn.Softmax(dim=1)   # used in line 47
+
+    while True:
+        obs_v = torch.tensor(obs, dtype=torch.float32).unsqueeze(0)
+        act_probs_v = sm(net(obs_v))
+        act_probs = act_probs_v.detach().numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs) # this is cross entropy
+        next_obs, reward, is_done, is_trunc,  _ = env.step(action)
+        episode_reward += float(reward)
+        step = EpisodeStep(observation=obs, action=action)
+        episode_steps.append(step)
+        if is_done or is_trunc:
+            e = Episode(reward = episode_reward, steps = episode_steps)
+            batch.append(e)
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs, _ = env.reset()
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        obs = next_obs
+
+
+def filter_batch(batch: tt.List[Episode], percentile: float) -> tt.Tuple[torch.FloatTensor, torch.LongTensor, float, float]:
+    rewards = list(map(lambda s: s.reward, batch))
+    reward_bound = float(np.percentile(rewards, percentile))
+    reward_mean = float(np.mean(rewards))
+
+    train_obs : tt.List[np.ndarray] = []
+    train_act : tt.List[int] = []
+
+    for episode in batch:
+        if episode.reward < reward_bound:
+            continue
+        train_obs.extend(map(lambda step: step.observation, episode.steps))
+        train_act.extend(map(lambda step: step.action, episode.steps))
+    
+    train_obs_v = torch.FloatTensor(np.vstack(train_obs))
+    train_act_v = torch.LongTensor(train_act)
+    return train_obs_v, train_act_v, reward_bound, reward_mean
+
+
+if __name__ == '__main__':
+    env = DiscreteOneHotWrapper(gym.make("FrozenLake-v1"))
+    obs_size = env.observation_space.shape[0]
+    n_actions = int(env.action_space.n)
+
+    net = Net(obs_size, HIDDEN_SIZE, n_actions)
+    print(net)
+
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params = net.parameters(), lr=0.01)
+    wandb.init(project="frozenlake_rl", name="cross_entropy")
+
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        optimizer.zero_grad()
+        action_scores_v = net(obs_v)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+        print("%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f" % (
+                iter_no, loss_v.item(), reward_m, reward_b))
+
+        wandb.log({"loss": loss_v.item(), "iter_no": iter_no})
+        wandb.log({"reward_mean": reward_m, "iter_no": iter_no})
+        wandb.log({"reward_bound": reward_b, "iter_no": iter_no})
+
+        if reward_m > 475:
+            print('Solved')
+            break
+
+    wandb.finish()
